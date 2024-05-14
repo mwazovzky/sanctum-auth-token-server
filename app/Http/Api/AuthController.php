42034afdbd0c0bbc1da6eb790c63e54de919d33f
Mike Wazovzky <mike.wazovzky@gmail.com>
2024-05-14 12:21:11 +0400

    Logout. Revoke token

--- app/Http/Api/AuthController.php
+++ app/Http/Api/AuthController.php
@@ -5,11 +5,13 @@ namespace App\Http\Api;
 use App\Http\Api\LoginUserRequest;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(LoginUserRequest $request)
+    public function login(LoginUserRequest $request): JsonResponse
     {
         $attributes = $request->validated();
 
@@ -29,5 +31,12 @@ class AuthController extends Controller
                 'token' => $token,
             ],
         ], 200);
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([], 204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use App\Http\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/user', fn (Request $request) => $request->user());
+});
